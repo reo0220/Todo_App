da67d66cc15fc813f3f1a48264fd873bb20a90f4
EC2 Default User <ec2-user@ip-172-31-41-233.us-west-2.compute.internal>
2022-05-13 15:34:01 +0000

    change layout create & edit

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.layouts')
 
-@section('title','タスク追加')
+@section('title','タスク作成')
 
 @section('content')
+
+<h1>タスク作成</h1>
 
 <!-- POSTリクエストでデータを/tasksに送信 -->
 <form method="POST" action="/tasks">
     {{ csrf_field() }}
-    <input type="text" name="task">
-    <input type="text" name="description">
-    <input type="submit">
-</form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">タイトル</label>
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="task">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">詳細</label>
+        <textarea class="form-control" name="description"></textarea>
+    </div>
+    <button type="submit" class="btn btn-outline-primary">タスク追加</button>
+ </form>
+ 
+ <a href="/tasks" class="btn btn-outline-secondary">タスク一覧へ戻る</a>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,12 +4,25 @@
 
 @section('content')
 
+<h1>タスク編集</h1>
+
 <form method="POST" action="/tasks/{{ $task->id }}">
     {{ csrf_field() }}
     <input type="hidden" name="_method" value="PUT">
-    <input type="text" name="task" value="{{ $task->task }}">
-    <input type="text" name="description" value="{{ $task->description }}">
-    <input type="submit">
-</form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">タイトル</label>
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="task" value="{{ $task->task }}">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">詳細</label>
+        <textarea class="form-control" name="description">{{ $task->description }}</textarea>
+    </div>
+    <button type="submit" class="btn btn-outline-primary">タスク編集</button>
+ </form>
+ 
+ <a href="/tasks" class="btn btn-outline-secondary">タスク一覧へ戻る</a>
+
+        
+   
 
 @endsection
