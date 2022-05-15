d63c4f62294a6a78fecbb3fc01c76398b0a61767
EC2 Default User <ec2-user@ip-172-31-41-233.us-west-2.compute.internal>
2022-05-15 14:59:22 +0000

    add viewport setting

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,6 +2,8 @@
     <head>
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
+       <!--レスポンシブ対応-->
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
        <!-- Bootstrap4の読み込み -->    
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
