2d3d6376de4841e117d42428c156172ad0dd2dfd
EC2 Default User <ec2-user@ip-172-31-41-233.us-west-2.compute.internal>
2022-05-16 14:16:20 +0000

    change footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-light fixed-bottom">
     <div class="container">
-        <p class="text-right">Reo Kodama</p>
+        <p class="text-center">Reo Kodama</p>
     </div>
 </footer>
