9d88e11688b9284fe230cbc13c5a3cf1f196c528
EC2 Default User <ec2-user@ip-172-31-41-233.us-west-2.compute.internal>
2022-05-18 12:58:06 +0000

    deploy to heroku

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -5,7 +5,7 @@
        <!--レスポンシブ対応-->
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
        <!-- Bootstrap4の読み込み -->    
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
        <div class="header mb-5">
@@ -22,6 +22,6 @@
         @component('components.footer')
         @endcomponent
         
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
